7191ca80924e819d0e7b6da924f78f90bd1ea615
João Vitor da Silva <jdalpicio@outlook.com>
2023-08-17 11:51:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -34,15 +34,43 @@ void numDiagonais (int linhas, int colunas){
    printf("diagonais: %d", diagonais);
 }
 
+int *alocaString(int linhas, int colunas){
+    int *v;
+
+    v = (int*) malloc(linhas*colunas*sizeof(int));
+    if(v==NULL){
+        printf("Memoria insuficiente. \n");
+        exit(1);
+    }
+    return v;
+}
+
+
 int main ()
 {
     int linhas, colunas;
-
+    int posicao = 0;
+    char *string;
     leLinhasColunas (&linhas, &colunas);
-
+    string = alocaString(linhas, colunas);
+    int tamanhoMatriz = linhas*colunas;
     printf ("linhas = %d, colunas = %d\n", linhas, colunas);
 
-    numDiagonais(linhas, colunas);
+    //numDiagonais(linhas, colunas);
+
+    printf("Digite uma String:  ");
+    fflush(stdin);
+    fgets(string, tamanhoMatriz+1, stdin);
+    int len = strlen(string);
+    printf("%s\n", string);
+    for (int i = 0; i < len; i++) {
+        if (string[i] == ' ') continue;
+        string[posicao] = string[i];
+        posicao++;
+    }
+    string[posicao] = '\0';
+    printf("String sem espacos em branco: %s\n", string);
 
 }
+
 
